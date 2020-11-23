39634380ca620f64438050a87cb40f2fbbd20117
Leonel Ubeda <leonelubeda@hotmail.com>
2020-11-23 00:06:27 -0600

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+    future: {
+        // removeDeprecatedGapUtilities: true,
+        // purgeLayersByDefault: true,
+    },
+    purge: [],
+    theme: {
+        screens: {
+            'sm': '640px',
+            // => @media (min-width: 640px) { ... }
+
+            'md': '768px',
+            // => @media (min-width: 768px) { ... }
+
+            'lg': '1024px',
+            // => @media (min-width: 1024px) { ... }
+
+            'xl': '1280px',
+            // => @media (min-width: 1280px) { ... }
+        },
+        extend: {
+            fontFamily: {
+                // display: '',
+                sans: ['Raleway', 'sans-serif']
+            },
+            borderWidth: {
+                '16': '16px'
+            },
+            height: {
+                '96': '24rem',
+                '128': '32rem',
+                '192': '48rem',
+
+            },
+            textColor: {
+                'nucleo-green': '#00a54f'
+            }
+        },
+    },
+    variants: {
+        container: [
+
+        ]
+    },
+    plugins: [],
+}
