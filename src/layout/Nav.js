6b427216775bdb55c9839a38f0984693038fc2bf
Leonel Ubeda <leonelubeda@hotmail.com>
2020-11-24 22:51:59 -0600

    add class to selected nav element

--- src/layout/Nav.js
+++ src/layout/Nav.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {Link, NavLink} from 'react-router-dom'
 
 
 const Nav = () => {
     return (
         <div className="flex justify-center w-full text-gray-600 border-t pb-1 py-2 bg-white">
-            <Link to="/">
+            <NavLink to="/search" activeClassName="selected-menu-item">
                 <div className="flex flex-col items-center mx-2">
                     <svg xmlns="http://www.w3.org/2000/svg" className="w-8" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none"
                          strokeLinecap="round" strokeLinejoin="round">
@@ -15,8 +15,8 @@ const Nav = () => {
                     </svg>
                     <span className="text-sm font-semibold text-gray-800">Search</span>
                 </div>
-            </Link>
-            <Link to="/">
+            </NavLink>
+            <NavLink to="/" exact activeClassName="selected-menu-item">
                 <div className="flex flex-col items-center mx-2">
                     <svg xmlns="http://www.w3.org/2000/svg" className="w-8" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none"
                          strokeLinecap="round" strokeLinejoin="round">
@@ -25,19 +25,19 @@ const Nav = () => {
                         <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7"/>
                         <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6"/>
                     </svg>
-                    <span className="text-sm font-semibold text-gray-800">Home</span>
+                    <span className="text-sm font-semibold">Home</span>
                 </div>
-            </Link>
-            <Link to="/">
+            </NavLink>
+            <NavLink to="/saved" activeClassName="selected-menu-item">
                 <div className="flex flex-col items-center mx-2">
                     <svg xmlns="http://www.w3.org/2000/svg" className="w-8" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" fill="none"
                          strokeLinecap="round" strokeLinejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                         <path d="M9 4h6a2 2 0 0 1 2 2v14l-5-3l-5 3v-14a2 2 0 0 1 2 -2"/>
                     </svg>
-                    <span className="text-sm font-semibold text-gray-800">Saved</span>
+                    <span className="text-sm font-semibold">Saved</span>
                 </div>
-            </Link>
+            </NavLink>
 
         </div>
     )
