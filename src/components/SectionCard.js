d0b717e13aea839dff203bcb418f3a6d44e5058d
Leonel Ubeda <leonelubeda@hotmail.com>
2020-11-24 22:57:30 -0600

    refactor SectionCard.js large viewport

--- src/components/SectionCard.js
+++ src/components/SectionCard.js
@@ -6,10 +6,10 @@ export default ({src, link, title, className=""}) => {
     return (
         <>
             <Link to={link}>
-                <div className="relative h-32 w-full">
-                    <div className="w-full h-full bg-black opacity-25 absolute"></div>
+                <div className="relative h-32 md:h-full w-full">
+                    <div className="w-full h-full bg-black opacity-25 absolute  rounded-xl"></div>
                     <h3 className="text-2xl font-bold absolute text-white ml-4 mt-2 text-shadow-lg z-10">{title}</h3>
-                    <img src={src} className="h-full w-full object-cover border-lg rounded-xl"/>
+                    <img src={src} className="h-full w-full object-cover border-lg rounded-xl transition duration-500 hover:scale-150"/>
                 </div>
             </Link>
 
