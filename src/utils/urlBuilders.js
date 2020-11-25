b3ab78f4c4d407574e938edae9118951346fee70
Leonel Ubeda <leonelubeda@hotmail.com>
2020-11-25 02:30:57 -0600

    add url builder for characters listing

--- src/utils/urlBuilders.js
+++ src/utils/urlBuilders.js
@@ -1,5 +1,5 @@
 import urlcat from 'urlcat'
-import {API_KEY, BASE_URL, COMIC_DETAIL_PATH, COMICS_PATH} from "../constants";
+import {API_KEY, BASE_URL, COMIC_DETAIL_PATH, COMICS_PATH, CHARACTERS_PATH} from "../constants";
 
 
 export const buildComicDetailURL = (id, params = {}) => {
@@ -8,4 +8,9 @@ export const buildComicDetailURL = (id, params = {}) => {
 
 export const buildComicsURL = (params={}) => {
     return urlcat(BASE_URL, COMICS_PATH, params = {...params, apikey: API_KEY})
+}
+
+
+export const buildCharactersURL = (params={}) => {
+    return urlcat(BASE_URL, CHARACTERS_PATH, params = {...params, apikey: API_KEY})
 }
