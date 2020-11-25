59ab7dd1ded62ad85376221a44d3b866f1541862
Leonel Ubeda <leonelubeda@hotmail.com>
2020-11-25 04:01:10 -0600

    refactor comic detail info

--- src/pages/ComicDetail.js
+++ src/pages/ComicDetail.js
@@ -3,9 +3,11 @@ import {useParams} from 'react-router-dom'
 import useMarvelFetch from '../hooks/useMarvelFetch'
 import {buildComicDetailURL} from '../utils/urlBuilders'
 import SectionHeader from "../layout/SectionHeader"
-import LoadingAnimation from "../components/LoadingAnimation";
-import DisplayPrices from "../components/DisplayPrices";
-import ErrorMessage from "../components/ErrorMessage";
+import LoadingAnimation from "../components/LoadingAnimation"
+import DisplayPrices from "../components/DisplayPrices"
+import ErrorMessage from "../components/ErrorMessage"
+import ComicDate from "../components/comics/ComicDate"
+import {nanoid} from "nanoid";
 
 export default () => {
     const { id } = useParams()
@@ -32,26 +34,52 @@ export default () => {
                     <ErrorMessage title={statusCode === 404 ? 'Not Found' : 'Application Error'} actionLink={"/"} actionTitle="Go home"/>
                 : null}
                 {isLoading ? <LoadingAnimation /> : null}
-                {element && !isLoading ?
-                    <div className="flex flex-col items-center mt-4 md:flex-row md:items-start md:justify-center md:max-w-3xl md:mx-auto">
+                {element && !isLoading && statusCode === 200?
+                    <div className="flex flex-col items-center mt-4 md:flex-row md:items-start md:justify-around md:max-w-3xl md:mx-auto">
                         <img src={`${element.thumbnail.path}.${element.thumbnail.extension}`}
                              className="h-96 object-cover rounded-md shadow-md"
                              alt=""/>
                         <div className="my-2 mx-3">
                             <h1 className="text-xl uppercase font-bold text-gray-700 my-2">{element.title}</h1>
 
+                            <div className="text-lg text-gray-700 font-semibold my-2">
+                                <h3 className="text-red-500">Details</h3>
+                                <div className="text-sm grid grid-cols-1 md:grid-cols-2">
+                                    {element.pageCount ?
+                                        <h5 className="mb-2">Pages: <span className="text-teal-500">{element.pageCount}</span></h5>
+                                    : null}
+                                    {element.issueNumber && element.issueNumber !== "" ?
+                                        <h5 className="mb-2">Issue number: <span className="text-green-500">{element.issueNumber}</span></h5>
+                                    : null}
+                                    {element.format ?
+                                        <h5 className="mb-2">Format: <span className="text-green-500">{element.format}</span></h5>
+                                    : null}
+
+                                    {element.dates ?
+                                        <>
+                                            {element.dates.map(e => <ComicDate type={e.type} date={e.date} key={nanoid(5)}/>)}
+                                        </>
+                                    : null}
+
+                                </div>
+                            </div>
+                            {element.prices.length > 0 ?
+                                <div className="my-2">
+                                    <h2 className="text-red-500 font-semibold">Price</h2>
+                                    <DisplayPrices prices={element.prices}/>
+                                </div>
+                            : null}
+
                             {element.description ?
-                                <p>{element.description}</p>
+                                <>
+                                    <h3 className="text-red-500 font-semibold">Description</h3>
+                                    <p>{element.description}</p>
+                                </>
                             :
                                 <h2 className="font-semibold text-xl text-red-500">No description!</h2>
                             }
 
-                            {element.prices.length > 0 ?
-                                <div className="my-2">
-                                    <h2 className="text-gray-600 font-semibold">Price</h2>
-                                    <DisplayPrices prices={element.prices}/>
-                                </div>
-                            : null}
+
                         </div>
                     </div>
                     : null}
