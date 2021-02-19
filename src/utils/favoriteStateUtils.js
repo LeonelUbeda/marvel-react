5d21a98dbe0863e1da3f25612d929cb667736854
Leonel Ubeda <leonelubeda@hotmail.com>
2021-02-19 15:15:32 -0600

    refactor root components and utils files

--- src/utils/favoriteStateUtils.js
+++ src/utils/favoriteStateUtils.js
@@ -1,4 +1,4 @@
+export const searchIfFavorite = (list, id, type) =>
+  list.some((e) => e.id === id && e.type === type);
 
-export const searchIfFavorite = (list , id, type) =>{
-    return list.some(e => e.id == id && e.type === type)
-}
+export default { searchIfFavorite };
