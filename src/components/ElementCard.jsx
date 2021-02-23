243cae4b3989fe146167a2df3d041e653168965d
Leonel Ubeda <leonelubeda@hotmail.com>
2021-02-23 16:22:45 -0600

    :sparkles: implement proptypes to ElementCard component

--- src/components/ElementCard.jsx
+++ src/components/ElementCard.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { nanoid } from 'nanoid';
+import PropTypes from 'prop-types';
 
-export default ({ link, title, image, extras }) => (
+const ExtraCard = ({ title, className }) => (
+  <span className={`px-2 py-1 rounded-md my-1 ${className}`} key={nanoid()}>
+    {title}
+  </span>
+);
+
+const ElementCard = ({ link, title, image, extras }) => (
   <Link to={link}>
     <div className="w-full flex flex-col items-center w-full h-full relative">
       <div className="w-auto relative">
@@ -11,12 +18,10 @@ export default ({ link, title, image, extras }) => (
           alt={`${title} cover`}
           className="h-96 w-auto md:h-64 lg:h-96 object-cover rounded-md shadow-md"
         />
-        {extras ? (
+        {extras.length > 0 ? (
           <div className="extras my-2 flex items-end w-full text-white text-xs absolute top-0 left-0 flex flex-col transform translate-x-3">
             {extras.map((extra) => (
-              <span className={`px-2 py-1 rounded-md my-1 ${extra.className}`} key={nanoid()}>
-                {extra.title}
-              </span>
+              <ExtraCard title={extra.title} className={extra.className} />
             ))}
           </div>
         ) : null}
@@ -27,3 +32,19 @@ export default ({ link, title, image, extras }) => (
     </div>
   </Link>
 );
+
+const extraShape = PropTypes.shape({
+  className: PropTypes.string,
+  title: PropTypes.string.isRequired,
+});
+
+ElementCard.propTypes = {
+  link: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  extras: PropTypes.arrayOf(extraShape),
+};
+
+ElementCard.defaultProps = { extras: [] };
+
+export default ElementCard;
