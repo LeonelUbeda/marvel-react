a461521b9de3c87ffeb58e063e89355a02e3ec1a
Leonel Ubeda <leonelubeda@hotmail.com>
2021-03-08 10:12:20 -0600

    remove unused import

--- src/components/__tests__/ElementCard.test.jsx
+++ src/components/__tests__/ElementCard.test.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import { renderWithMemoryRouter } from '../../utils/testUtils';
 import ElementCard from '../ElementCard';
 
