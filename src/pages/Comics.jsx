1e73a4e0797eb55864f8a62479d27a2b273b87a8
Leonel Ubeda <leonelubeda@hotmail.com>
2021-03-11 11:15:33 -0600

    implement store to Comic page

--- src/pages/Comics.jsx
+++ src/pages/Comics.jsx
@@ -1,23 +1,22 @@
 import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 import { nanoid } from 'nanoid';
+import { getItemsFromStore } from '../utils/store';
 import ElementCard from '../components/ElementCard';
 import Pagination from '../components/Pagination';
-import useMarvelFetch from '../hooks/useMarvelFetch';
-import { buildComicsURL } from '../utils/urlBuilders';
-
+import { setParams } from '../store/comics/comics.actions';
+import LoadingHandler from '../components/LoadingHandler';
+import ErrorHandler from '../components/ErrorHandler';
 import DynamicFilter from '../components/DynamicFilter';
 import SectionHeader from '../layout/SectionHeader';
-import LoadingAnimation from '../components/LoadingAnimation';
-import ErrorMessage from '../components/ErrorMessage';
+import SimpleArrow from '../components/SimpleArrow';
 import GenericMessage from '../components/GenericMessage';
 
 function useQuery() {
   return new URLSearchParams(useLocation().search);
 }
-
 const LIMIT = 20;
-
 const FILTERS = [
   {
     label: 'Order by',
@@ -46,22 +45,58 @@ const FILTERS = [
     ],
   },
 ];
+const ComicItem = ({ characters, format, thumbnail, id, title }) => {
+  const extras = [];
+  if (characters && characters.available > 0) {
+    extras.push({
+      className: 'bg-red-500',
+      title: `${characters.available} characters`,
+    });
+  }
+  if (format) {
+    extras.push({
+      className: 'bg-green-500',
+      title: format,
+    });
+  }
+  return (
+    <ElementCard
+      key={nanoid()}
+      image={`${thumbnail.path}.${thumbnail.extension}`}
+      link={`/comics/${id}`}
+      title={title}
+      extras={extras}
+    />
+  );
+};
+
+function getPageFromQuery(value) {
+  const temp = parseInt(value, 10);
+  if (!Number.isNaN(temp) && temp > 0) {
+    return temp;
+  }
+  return 1;
+}
 
 export default () => {
   const query = useQuery();
   const [filters, setFilters] = useState({});
-  const [apiURL, setApiURL] = useState('');
-  const [selectedPage, setSelectedPage] = useState(
-    parseInt(query.get('page'), 10)
-  );
-  const { isLoading, elements, statusCode, pagesQuantity } = useMarvelFetch(
-    apiURL,
-    {}
-  );
+  const dispatch = useDispatch();
   const [isFilterHidden, setIsFilterHidden] = useState(true);
+  const [items, setItems] = useState([]);
+  const {
+    items: itemsFromStore,
+    isLoading,
+    error,
+    currentRequest,
+  } = useSelector((state) => state.comics);
+
+  const [selectedPage, setSelectedPage] = useState(
+    getPageFromQuery(query.get('page'))
+  );
 
   function handlePageClick({ selected }) {
-    setSelectedPage(selected);
+    setSelectedPage(selected + 1);
   }
 
   function toggleIsFilterHidden() {
@@ -69,43 +104,25 @@ export default () => {
   }
 
   function filterChangeHandler(newFilterObject) {
+    setSelectedPage(1);
     setFilters(newFilterObject);
   }
-
-  // change apiURL if filters or page changes
-  // if apiURL changes, useMarvelFetch will make a request with the new url
   useEffect(() => {
-    setApiURL(
-      buildComicsURL({ ...filters, limit: LIMIT, offset: LIMIT * selectedPage })
-    );
+    dispatch(setParams({ page: selectedPage, limit: LIMIT, filters }));
   }, [filters, selectedPage]);
 
+  useEffect(() => {
+    setItems(getItemsFromStore(itemsFromStore, currentRequest.items));
+  }, [currentRequest.items]);
+
   return (
-    <div className="">
+    <div>
       <SectionHeader>
         <h3 className="text-2xl font-bold py-2 text-gray-100">Comics</h3>
         <h3 className="text-xl font-bold py-2 text-gray-100 cursor-pointer">
           <button type="button" onClick={toggleIsFilterHidden}>
             Filters
-            <span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className={`w-6 inline transition duration-200 ease-in-out transform ${
-                  !isFilterHidden ? 'rotate-180' : null
-                }`}
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <line x1="12" y1="5" x2="12" y2="19" />
-                <line x1="18" y1="11" x2="12" y2="5" />
-                <line x1="6" y1="11" x2="12" y2="5" />
-              </svg>
-            </span>
+            <SimpleArrow isUp={isFilterHidden} />
           </button>
         </h3>
       </SectionHeader>
@@ -119,63 +136,49 @@ export default () => {
       </div>
 
       <div className="container mx-auto py-5">
-        {isLoading ? <LoadingAnimation /> : null}
-
-        {!isLoading && statusCode !== 200 ? (
-          <ErrorMessage
-            title="Application Error"
-            actionLink="/"
-            actionTitle="Go home"
-          />
-        ) : null}
-
-        {elements && !isLoading && statusCode === 200 ? (
-          <>
-            <div className="grid grid-cols-1 md:grid-cols-4 gap-x-4 gap-y-8 px-2">
-              {elements.length === 0 ? (
-                <GenericMessage
-                  title="No elements"
-                  subTitle="Try a different search"
-                />
-              ) : null}
-
-              {elements.map((element) => {
-                const extras = [];
-                if (element.characters && element.characters.available > 0) {
-                  extras.push({
-                    className: 'bg-red-500',
-                    title: `${element.characters.available} characters`,
-                  });
-                }
-                if (element.format) {
-                  extras.push({
-                    className: 'bg-green-500',
-                    title: element.format,
-                  });
-                }
-                return (
-                  <ElementCard
-                    key={nanoid()}
-                    image={`${element.thumbnail.path}.${element.thumbnail.extension}`}
-                    link={`/comics/${element.id}`}
-                    title={element.title}
-                    extras={extras}
-                  />
-                );
-              })}
-            </div>
-
-            {elements.length > 0 ? (
-              <div className="container mx-auto pb-20 py-10">
+        <ErrorHandler error={error}>
+          <LoadingHandler isLoading={isLoading}>
+            <>
+              <div className="container mx-auto py-5">
                 <Pagination
                   handlePageClick={handlePageClick}
-                  pagesQuantity={pagesQuantity}
+                  pagesQuantity={currentRequest.total / LIMIT}
                   selectedPage={selectedPage}
                 />
               </div>
-            ) : null}
-          </>
-        ) : null}
+              <div className="grid grid-cols-1 md:grid-cols-4 gap-x-4 gap-y-8 px-2">
+                {items.length === 0 ? (
+                  <GenericMessage
+                    title="No elements"
+                    subTitle="Try a different search"
+                  />
+                ) : (
+                  <>
+                    {items.map(
+                      ({ characters, format, thumbnail, id, title }) => (
+                        <ComicItem
+                          characters={characters}
+                          format={format}
+                          thumbnail={thumbnail}
+                          id={id}
+                          title={title}
+                          key={nanoid()}
+                        />
+                      )
+                    )}
+                  </>
+                )}
+              </div>
+              <div className="container mx-auto pb-20 py-10">
+                <Pagination
+                  handlePageClick={handlePageClick}
+                  pagesQuantity={currentRequest.total / LIMIT}
+                  selectedPage={selectedPage}
+                />
+              </div>
+            </>
+          </LoadingHandler>
+        </ErrorHandler>
       </div>
     </div>
   );
