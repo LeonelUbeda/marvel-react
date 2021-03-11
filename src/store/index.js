10e4d745b2f2bfeb7f4ac6259196ea6d9d74854b
Leonel Ubeda <leonelubeda@hotmail.com>
2021-03-11 13:23:44 -0600

    add character reducer to index

--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,12 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import favoriteReducer from './favorite/favorite.reducer';
 import comicsReducer from './comics/comics.reducer';
+import charactersReducer from './characters/characters.reducer';
 
 const combinedReducers = combineReducers({
   favorite: favoriteReducer,
   comics: comicsReducer,
+  characters: charactersReducer,
 });
 
 export default createStore(
