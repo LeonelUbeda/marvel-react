70a31d3a5740dcf65dde150228eba198b690f8c4
Leonel Ubeda <leonelubeda@hotmail.com>
2021-03-11 11:14:42 -0600

    add comic reducer to index

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,12 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import favoriteReducer from './favorite/favorite.reducer';
+import comicsReducer from './comics/comics.reducer';
 
-const combinedReducers = combineReducers({ favorite: favoriteReducer });
+const combinedReducers = combineReducers({
+  favorite: favoriteReducer,
+  comics: comicsReducer,
+});
 
 export default createStore(
   combinedReducers,
