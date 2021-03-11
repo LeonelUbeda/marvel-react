0ec923ca9baf0f007d13b39fe17cd272d99237b6
Leonel Ubeda <leonelubeda@hotmail.com>
2021-03-11 14:09:45 -0600

    :rotating_light: fix linter warnings

--- src/components/SimpleArrow.jsx
+++ src/components/SimpleArrow.jsx
@@ -10,8 +10,7 @@ export default ({ isUp, sizeClassName = 'w-6' }) => (
     stroke="currentColor"
     fill="none"
     strokeLinecap="round"
-    strokeLinejoin="round"
-  >
+    strokeLinejoin="round">
     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
     <line x1="12" y1="5" x2="12" y2="19" />
     <line x1="18" y1="11" x2="12" y2="5" />
