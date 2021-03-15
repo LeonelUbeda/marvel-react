8f12e897bfa43f013e245aae02f769ab352b0837
Leonel Ubeda <leonelubeda@hotmail.com>
2021-03-15 11:12:07 -0600

    :boom: implement errors store in ErrorHandler

--- src/components/ErrorHandler.jsx
+++ src/components/ErrorHandler.jsx
@@ -1,33 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
+import { removeError } from '../store/errors/errors.actions';
 import ErrorMessage from './ErrorMessage';
 
-const ErrorHandler = ({ children, error }) => (
-  <>
-    {error ? (
-      <ErrorMessage
-        title="Application Error"
-        actionLink="/"
-        actionTitle="Go home"
-      />
-    ) : (
-      children
-    )}
-  </>
-);
+const ErrorHandler = ({ children }) => {
+  const { error } = useSelector((state) => state.errors);
+  const dispatch = useDispatch();
+  useEffect(
+    () => () => {
+      dispatch(removeError());
+    },
+    []
+  );
+  return (
+    <>
+      {error ? (
+        <ErrorMessage
+          title={error.message || 'Application Error'}
+          actionLink="/"
+          actionTitle="Go home"
+        />
+      ) : (
+        children
+      )}
+    </>
+  );
+};
 
 ErrorHandler.propTypes = {
   children: PropTypes.node.isRequired,
-  error: PropTypes.oneOf([
-    PropTypes.shape({
-      message: PropTypes.string.isRequired,
-    }),
-    null,
-  ]),
-};
-
-ErrorHandler.defaultProps = {
-  error: null,
 };
 
 export default ErrorHandler;
